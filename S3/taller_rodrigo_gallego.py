8d39d62ca2b0e958c5924dd91124de1ff5e98e95
RodrigoDEVS <rodrigo.gallego@outlook.es>
2026-03-30 22:22:48 -0500

    feat: :sparkles: Refactor pseudoagente para mejorar la gestión de comandos, agregar nuevas funcionalidades y capturar excepciones.

--- S3/taller_rodrigo_gallego.py
+++ S3/taller_rodrigo_gallego.py
@@ -62,17 +62,9 @@ historial_chat: MemoriaAgente = []
 mensaje = ""
 comando_valido = True # Se agrega esta variable para controlar cuando se guarda el log y cuando no
 sistema_activo = True
-while sistema_activo:
-    cmd = input("""Seleccione un comando para el pseudoagente:
-- 'salir' para terminar la sesión
-- 'ping' para recibir un pong
-- 'contar' para contar letras, vocales y consonantes en una palabra
-- 'fecha_hoy' para mostrar la fecha y hora actual
-- 'validar_pass' para validar la fortaleza de una contraseña
-- 'calculadora' para realizar operaciones matemáticas básicas
-- 'historial' para mostrar el historial de comandos
-Agente: """).strip().lower()
 
+def pseudo_action(cmd: str, rol: str, historial: MemoriaAgente) -> None:
+    global sistema_activo, mensaje, comando_valido
     if cmd == "salir":
         print("------Agente apagado. Vuelve pronto.------")
         sistema_activo = False
@@ -80,26 +72,10 @@ Agente: """).strip().lower()
     elif cmd == "ping":
         print("pong.")
         mensaje = "Se ha enviado un ping y de respuesta se devolvió un pong."
-    elif cmd =="contar":
-        palabra = input("Ingrese una palabra: ").lower()
-        tot_letras = len(palabra)
-        tot_vocales = 0
-        tot_cons = 0
-        
-        for p in palabra:
-            if p in "aeiou":
-                tot_vocales +=1
-            else:
-                tot_cons +=1
-        
-        print(f"Palabra ingresada: {palabra}")
-        print(f"Total de vocales: {tot_vocales}")
-        print(f"Total de consonantes: {tot_cons}")
-        print(f"Total de letras: {tot_letras}")
-        mensaje = f"""Se solicitó el conteo de la palabra {palabra}, dando como resultados:
-            Vocales: {tot_vocales}
-            Consonantes: {tot_cons}
-            Total: {tot_letras}"""
+    elif cmd == "contar":
+        palabra = input("Ingrese una palabra: ").strip().lower()
+        mensaje = contar_letras(palabra)
+        print(mensaje)
     # Nueva funcionalidad: Mostrar fecha y hora actual
     elif cmd == "fecha_hoy":
         # Se obtiene la fecha y hora actual formateada como "DD/MM/YYYY HH:MM:SS"
@@ -110,94 +86,131 @@ Agente: """).strip().lower()
             mensaje = f"[PseudoAgente] La fecha y hora actual es: {fecha_hora_actual}"
         else:
             mensaje = "[Acceso Denegado] Este comando requiere privilegios de administrador."  
-            print("[Acceso Denegado] Este comando requiere privilegios de administrador.")
+            # print("[Acceso Denegado] Este comando requiere privilegios de administrador.")
+            raise PermissionError("Acceso Denegado - Privilegios insuficientes.")
     # Nueva funcionalidad: Validar fortaleza de una contraseña
     elif cmd == "validar_pass":
         contrasena_a_validar = input("Ingrese la contraseña a validar: ").strip()
         # Se define la fortaleza de la contraseña basada en su longitud y la presencia de caracteres especiales
-        if len(contrasena_a_validar) >= 8 and any(char in "!@#$%^&*()-_=+[]{}|;:'\",.<>?/" for char in contrasena_a_validar):
-            mensaje = "La contraseña es fuerte."
-            print("La contraseña es fuerte.")
-        elif len(contrasena_a_validar) >= 6:
-            mensaje = "La contraseña es moderada."
-            print("La contraseña es moderada.")
-        elif contrasena_a_validar == usuario:
-            mensaje = "La contraseña no debe ser igual al nombre de usuario."
-            print("La contraseña no debe ser igual al nombre de usuario.")
-        else:
-            mensaje = "La contraseña es débil."
-            print("La contraseña es débil.")
-
+        mensaje = validar_pass(contrasena_a_validar)
+        print(mensaje)
     # Función de calculadora
     elif cmd == "calculadora":
-        print("Ingrese la operación en formato: número1 operador número2 (ejemplo: 5 + 3)")
-        num1 = input("Ingrese el primer número: ").strip()
-        operador = input("Ingrese el operador (+, -, *, /): ").strip()
-        num2 = input("Ingrese el segundo número: ").strip()
         try:
-            num1 = float(num1)
-            num2 = float(num2)
-            if operador == "+":
-                resultado = num1 + num2
-            elif operador == "-":
-                resultado = num1 - num2
-            elif operador == "*":
-                resultado = num1 * num2
-            elif operador == "/":
-                if num2 != 0:
-                    resultado = num1 / num2
-                else:
-                    print("Error: División por cero no permitida.")
-                    continue
-            
-            else:
-                print("Operador no reconocido. Use +, -, *, o /.")
-                continue
-            mensaje = f"Se realizó la operación: {num1} {operador} {num2} = {resultado}"
-            print(f"Resultado: {resultado}")
-            # Captura de errores para entradas no numéricas o formato incorrecto
-        except ValueError:
-            print("Formato de operación incorrecto. Intente de nuevo.")
+            resultado = calculadora()
+            mensaje = f"Resultado: {resultado}"
+            print(mensaje)
+        except ValueError as ex:
+            mensaje = f"Error: {ex}"
+            print(mensaje)
     elif cmd == "historial":
         historial = input("""Historial de Comandos:
 - 'all' ver todo el historial
 - 'clear' limpiar el historial
 - 'buscar' buscar en el historial por palabra clave
 """)
-        if historial == "all":
-            if not historial_chat:
-                print("El historial está vacío.")
-            else:
-                print("Historial completo:")
-                for entry in historial_chat:
-                    print(entry)
-        elif historial == "clear":
-            historial_chat.clear()
-            print("Se limpió el historial.")
-        elif historial == "buscar":
-            # Se almacena la entrada de busqueda, se limpian los espacios y se convierte a minusculas para facilitar la búsqueda
-            keyword = input("Ingrese la palabra clave para buscar en el historial: ").strip().lower()
-            # Se itera sobre el historial_chat para encontrar entradas que contengan la palabra clave "keyword" en el comando o en la descripción, se almacena el resultado en una nueva lista 'resultados'
-            resultados: MemoriaAgente = [entry for entry in historial_chat if keyword in entry['cmd'] or keyword in entry['descripcion']]
-            if resultados:
-                print(f"Resultados de búsqueda para '{keyword}':")
-                for entry in resultados:
-                    print(entry)
-            else:
-                # Si no se encuentran resultados, se muestra un mensaje indicando que no se encontraron coincidencias
-                print(f"No se encontraron entradas en el historial que contengan '{keyword}'.")
-        comando_valido = False # No se guarda el log para el comando de historial, ya que es una consulta al mismo
+        log: str = gestionar_historial(historial_chat, historial)
+        print(log)
     else:
         print("------Comando desconocido. Intente de nuevo.-------")
         comando_valido = False
     
-    #TO-DO: Taller de la semana - Búsqueda de memoria
+    almacenar_log(cmd, rol, mensaje, historial)
+
+def almacenar_log(cmd: str, rol: str, descripcion: str, historial: MemoriaAgente) -> None:
+    global comando_valido, mensaje
     if comando_valido:
         d_log: Recuerdo = {"timestamp": datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
                 "cmd": cmd,
-                "rol": rol_actual,
-                "descripcion": mensaje}
+                "rol": rol,
+                "descripcion": descripcion}
                     
-        historial_chat.append(d_log)
+        historial.append(d_log)
     comando_valido = True # Se resetea la variable para el siguiente comando
     mensaje = "" # Se resetea el mensaje para el siguiente comando
+
+def gestionar_historial(historial: MemoriaAgente, accion: str) -> str:
+    global comando_valido
+    log: str = ""
+    if accion == "all":
+        if not historial:
+            log = "El historial está vacío."
+        else:
+            log += f"Historial completo:\n"
+            for entry in historial:
+                # se retorna el log con el historial completo entrada por entrada
+                log += f"{entry}\n"
+                
+    elif accion == "clear":
+        historial.clear()
+        log = "Se limpió el historial."
+    elif accion == "buscar":
+        # Se almacena la entrada de busqueda, se limpian los espacios y se convierte a minusculas para facilitar la búsqueda
+        keyword = input("Ingrese la palabra clave para buscar en el historial: ").strip().lower()
+        # Se itera sobre el historial_chat para encontrar entradas que contengan la palabra clave "keyword" en el comando o en la descripción, se almacena el resultado en una nueva lista 'resultados'
+        resultados: MemoriaAgente = [entry for entry in historial if keyword in entry['cmd'] or keyword in entry['descripcion']]
+        if resultados:
+            log += f"Resultados de búsqueda para '{keyword}':\n"
+            for entry in resultados:
+                log += f"{entry}\n"
+        else:
+            log=f"No se encontraron entradas en el historial que contengan '{keyword}'."
+    comando_valido = False # No se guarda el log para el comando de historial, ya que es una consulta al mismo
+    return log
+
+def contar_letras(palabra: str) -> str:
+    # quitar los espacios de la palabra para contar solo las letras
+    palabra = palabra.replace(" ", "")
+    tot_letras = len(palabra)
+    tot_vocales = sum(1 for p in palabra if p in "aeiou")
+    tot_consonantes = tot_letras - tot_vocales
+    return f"Total de letras: {tot_letras}, Vocales: {tot_vocales}, Consonantes: {tot_consonantes}"
+
+def calculadora() -> str:
+    num1 = input("Ingrese el primer número: ").strip()
+    operador = input("Ingrese el operador (+, -, *, /): ").strip()
+    num2 = input("Ingrese el segundo número: ").strip()
+    try:
+        num1 = float(num1)
+        num2 = float(num2)
+    except ValueError:
+        raise ValueError("Por favor, ingrese números válidos.")
+
+    if operador == "+":
+        return str(num1 + num2)
+    elif operador == "-":
+        return str(num1 - num2)
+    elif operador == "*":
+        return str(num1 * num2)
+    elif operador == "/":
+        if num2 != 0:
+            return str(num1 / num2)
+        else:
+            raise ValueError("División por cero no permitida.")
+    else:
+        raise ValueError("Operador no reconocido. Use +, -, *, o /.")
+
+def validar_pass(contrasena: str) -> str:
+    if len(contrasena) >= 8 and any(char in "!@#$%^&*()-_=+[]{}|;:'\",.<>?/" for char in contrasena):
+        return "La contraseña es fuerte."
+    elif len(contrasena) >= 6:
+        return "La contraseña es moderada."
+    elif contrasena == usuario:
+        return "La contraseña no debe ser igual al nombre de usuario."
+    else:
+        return "La contraseña es débil."
+
+while sistema_activo:
+    try:
+        cmd = input("""Seleccione un comando para el pseudoagente:
+- 'salir' para terminar la sesión
+- 'ping' para recibir un pong
+- 'contar' para contar letras, vocales y consonantes en una palabra
+- 'fecha_hoy' para mostrar la fecha y hora actual
+- 'validar_pass' para validar la fortaleza de una contraseña
+- 'calculadora' para realizar operaciones matemáticas básicas
+- 'historial' para mostrar el historial de comandos
+Agente: """).strip().lower()
+        pseudo_action(cmd, rol_actual, historial_chat)
+    except Exception as e:
+        print(f"[Error] {e}")
